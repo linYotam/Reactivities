908aa0643f271a1297ae7836778301f4547ba4d1
Yotam Lin <linyotam@gmail.com>
2023-11-30 15:49:20 +0200

    end of section 4

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -6,5 +7,9 @@ namespace API.Controllers
     [Route("api/[controller]")] 
     public class BaseApiController : ControllerBase
     {
+        private IMediator _mediator; 
+
+        // If _mediator not exist -> create new mediator service
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,20 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
-
+// Add all new services to application services
+builder.Services.AddApplicationServices(builder.Configuration);
+ 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -35,7 +26,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
+var services = scope.ServiceProvider; 
 
 try
 {
